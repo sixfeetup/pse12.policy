ca7b5618d2434c7acff8efa19cad67f6e3c12b96
Clayton Parker <clayton@sixfeetup.com>
2012-05-19 21:30:04 -0400

    little PEP8 never hurt anyone

--- src/pse12/policy/testing.py
+++ src/pse12/policy/testing.py
@@ -1,10 +1,10 @@
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import FunctionalTesting
 from plone.app.testing import applyProfile
 
 from zope.configuration import xmlconfig
+
 
 class Pse12Policy(PloneSandboxLayer):
 
@@ -17,11 +17,13 @@ class Pse12Policy(PloneSandboxLayer):
                        pse12.policy,
                        context=configurationContext)
 
-
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'pse12.policy:default')
 
+
 PSE12_POLICY_FIXTURE = Pse12Policy()
-PSE12_POLICY_INTEGRATION_TESTING = \
-    IntegrationTesting(bases=(PSE12_POLICY_FIXTURE, ),
-                       name="Pse12Policy:Integration")
+PSE12_POLICY_INTEGRATION_TESTING = (
+    IntegrationTesting(
+        bases=(PSE12_POLICY_FIXTURE, ),
+        name="Pse12Policy:Integration")
+)
